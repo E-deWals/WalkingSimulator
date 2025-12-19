9ea476e78f32cb06767e8bbc2e5fea3313247a9a
Evelien <edewals2009@gmail.com>
2025-12-19 14:41:24 +0100

    animation of the fox works and worked on the meteor animation

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -18,6 +18,8 @@ public class FollowPlayer : MonoBehaviour
          rb = transform.parent.GetComponent<Rigidbody>();
     }
 
+
+    bool wasFoxWalking = false;
     // Update is called once per frame
     void Update()
     {
@@ -25,29 +27,52 @@ public class FollowPlayer : MonoBehaviour
         { 
             float distance = Vector3.Distance(transform.parent.position, player.position);
 
-            if (distance > maxDistance)
+            bool isFoxWalking = animator.GetBool("IsWalking");
+            if (isFoxWalking != wasFoxWalking)
+            {
+                Debug.Log("isFoxWalking = " + isFoxWalking);
+                wasFoxWalking = isFoxWalking;
+            }
+
+            //if (distance > maxDistance)
+            //{
+            //    transform.parent.LookAt(player.position);
+            //    animator.SetBool("IsWalking", true);
+            //    Vector3 direction = (player.position - transform.parent.position).normalized;
+            //    rb.linearVelocity = Vector3.Lerp(rb.linearVelocity, direction * speed, Time.deltaTime * 6);
+            //}
+            //else if (distance < minDistance)
+            //{
+            //    transform.parent.LookAt(player.position);
+            //    Vector3 reversedDirection = (player.position - transform.parent.position).normalized;
+            //    rb.linearVelocity = Vector3.Lerp(rb.linearVelocity, reversedDirection * speed, Time.deltaTime * 6);
+            //    animator.SetBool("IsWalking", true);
+            //}
+            //else
+            //{
+            //    transform.parent.LookAt(player.position);
+            //    rb.linearVelocity = Vector3.Lerp(rb.linearVelocity, Vector3.zero, Time.deltaTime * 6);
+            //    animator.SetBool("IsWalking", rb.linearVelocity.magnitude < 0.1f);
+
+            //}
+
+            if (distance > minDistance)
             {
                 transform.parent.LookAt(player.position);
+                //animator.SetBool("IsWalking", true);
                 Vector3 direction = (player.position - transform.parent.position).normalized;
+                direction.y = 0;
                 rb.linearVelocity = Vector3.Lerp(rb.linearVelocity, direction * speed, Time.deltaTime * 6);
-                animator.SetBool("IsWalking", true);
             }
-
-            if (distance < minDistance)
-            {
-                transform.parent.LookAt(player.position);
-                Vector3 reversedDirection = (player.position - transform.parent.position).normalized;
-                rb.linearVelocity = Vector3.Lerp(rb.linearVelocity, reversedDirection * speed, Time.deltaTime * 6);
-                animator.SetBool("IsWalking", true);
-            }
-
             else
             {
                 transform.parent.LookAt(player.position);
                 rb.linearVelocity = Vector3.Lerp(rb.linearVelocity, Vector3.zero, Time.deltaTime * 6);
-                animator.SetBool("IsWalking", false);
+                rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
+               
 
             }
+            animator.SetBool("IsWalking", rb.linearVelocity.magnitude > 0.1f);
         }
     }
 }
